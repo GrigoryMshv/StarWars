354fa187b6cf26fe7098c650bc122b40f3b15990
Grigory Moshev <grogory.mshv@gmail.com>
2020-04-26 17:18:34 +0300

    Homework1

--- core/src/ru/geekbrains/StarWars.java
+++ core/src/ru/geekbrains/StarWars.java
@@ -9,19 +9,20 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class StarWars extends ApplicationAdapter {
 	SpriteBatch batch; //передаёт в процессор текстуры
 	Texture img; //отвечает за работу с текстурами
+	static Texture background;
 	
 	@Override  //иницилизация всех классов с которыми будем работать
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+        background = new Texture("fonstarwars.jpg");
 	}
 
 	@Override
 	public void render () {   //метод срабатывает 60fps
-		Gdx.gl.glClearColor(1, 0, 0, 1); //цвет фона
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); //что это?
 		batch.begin(); //начало отрисовки
-		batch.draw(img, 0, 0);
+        batch.draw(background, 0f, 0f, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.draw(img, 0f, 0f);
 		batch.end(); //конец отрисовки
 	}
 	
@@ -29,5 +30,6 @@ public class StarWars extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose(); //выгружает всё лишнее из памяти
 		img.dispose(); //выгружает из памяти текстуры?
+        background.dispose();
 	}
 }
